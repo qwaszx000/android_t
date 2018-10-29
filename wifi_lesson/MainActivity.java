edb6d72b9166910ec37a4f7a666658e51a4d206f
qwaszx000 <37383968+qwaszx000@users.noreply.github.com>
2018-10-29 21:52:46 +0200

    Add files via upload

--- wifi_lesson/MainActivity.java
+++ wifi_lesson/MainActivity.java
@@ -55,7 +55,10 @@ public class MainActivity extends AppCompatActivity {
             if( !isSuccess ){
                 errorView.append("Error while connecting\r\n");
             }else if(isSuccess){
-                errorView.append(wifi.getConnectionInfo().toString());
+                String conInfo[] = wifi.getConnectionInfo().toString().split(",");
+                for(String i : conInfo) {
+                    errorView.append(i+"\r\n");
+                }
             }
         }else if(knownConnectionSuccess){
             errorView.append(wifi.getConnectionInfo().toString());
